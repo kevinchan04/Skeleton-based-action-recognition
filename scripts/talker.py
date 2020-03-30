58ad02e0960895c77bd2258e6c5a15f47625f71f
dongjai <13143532636@163.com>
2020-03-30 16:57:41 +0800

    add server.py which is ready for remote server process.

--- scripts/talker.py
+++ scripts/talker.py
@@ -18,7 +18,8 @@ from Modules.detection import DetectionLoader
 from Modules.poser import  PoseLoader
 from Modules.midprocessor import MidProcessor
 from Modules.recognizer import ActionRecognizer
-from Modules.act_talker import Talker
+# from Modules.act_talker import Talker
+from Modules.myserver import myServer
 
 def getTime(time1=0):
     if not time1:
@@ -48,7 +49,7 @@ parser.add_argument('--fps', type=int, default=30,
                         help='camera fps')
 parser.add_argument('--sp', default=True, action='store_true', #多线程系统显存不足
                     help='Use single process')
-parser.add_argument('--d435', type=bool, default=True,
+parser.add_argument('--d435', type=bool, default=False,
                         help='Using the D435 cam')
 parser.add_argument('--yolo_gpu_usages', type=float, default=0.4,
                     help='restrict the gpu memory')
@@ -60,9 +61,13 @@ parser.add_argument('--kp_num', type=list, default=[0, 4, 7, 9, 12],
                     help='the kps be used in recognizor')
 parser.add_argument('--yolo_weights', type=str, default=r"/home/dongjai/catkin_ws/src/act_recognizer/src/checkpoints/YOLO/yolov3.weights",
                     help='yolov3 weights')
+parser.add_argument('--host', type=str, default='10.60.2.65',
+                    help='server_host')
+parser.add_argument('--port', type=int, default=10086,
+                    help='server_port')
 
-# args = parser.parse_args()
-args, unknown = parser.parse_known_args()#NOTE : using in ROS environment
+args = parser.parse_args()
+# args, unknown = parser.parse_known_args()#NOTE : using in ROS environment
 # label_dict = {0: "walk", 1: "stop", 2: "come", 3: "phone", 4: "open", 5: "stand"}
 label_list = ["walk", "stop", "come", "phone", "open", "stand"]
 #For data statstic
@@ -73,9 +78,13 @@ mid_proc_time, mlp_pred_time = 0, 0
 det_loader_put_time, det_time = 0, 0
 pose_loader_put_time, det_openpose_time = 0, 0
 
+#Start the Server to receive data from client
+proc_server = myServer(args)
+proc_server.start()
+
 #Start the Camera Thread
-camera_reader = CamReader(args)
-camera_reader.start()
+# camera_reader = CamReader(args)
+# camera_reader.start()
 
 #Start the Image Reader Thread
 # image_reader = ImageReader(args)
@@ -98,26 +107,29 @@ act_recognizer = ActionRecognizer(args)
 act_recognizer.start()
 
 #Start the ROS Talker Thread to publish recognition data
-ros_talker = Talker(args)
-ros_talker.start()
+# ros_talker = Talker(args)
+# ros_talker.start()
 
 if args.d435:
     print('Starting  D435, press Ctrl + C to terminate...')
     sys.stdout.flush()
     im_names_desc = tqdm(loop())
 else:
-    data_len = image_reader.rgb_len
+    data_len = 2000
+    # data_len = image_reader.rgb_len
     im_names_desc = tqdm(range(data_len), dynamic_ncols=True)#终端输出
 
 try:
     for i in im_names_desc:
         pred_data = None
         start_time = getTime()
-        (rgb_nums, rgb_frame) = camera_reader.read_rgb()
+
+        (rgb_name, rgb_nums, rgb_frame, dp_name, dp_nums, dp_frame) = proc_server.read_data()
+        # (rgb_nums, rgb_frame) = camera_reader.read_rgb()
         # (rgb_name, rgb_nums, rgb_frame) = image_reader.read_rgb()
         # ckpt_time, rgb_read_time = getTime(start_time)
 
-        (dp_nums, dp_frame) = camera_reader.read_dp()
+        # (dp_nums, dp_frame) = camera_reader.read_dp()
         # (dp_name, dp_nums, dp_frame) = image_reader.read_dp()
         ckpt_time, img_read_time = getTime(start_time)
 
@@ -148,7 +160,7 @@ try:
                 # cv2.imwrite("/home/dongjai/catkin_ws/src/act_recognizer/store/random/{}.png".format(rgb_nums), image)
                 print(pred_data)
                 #Using ROS talker to publish the information.
-                ros_talker.put_data(pred_data)
+                # ros_talker.put_data(pred_data)
                 for i in pred_data:
                     action_t = i[-1]
                     if action_t == 0:
@@ -165,6 +177,7 @@ try:
                         open_t +=1   
             else:
                 pred_data = None
+        proc_server.get_data(pred_data)
         image = utils.draw_bbox(rgb_frame, bboxes, pred=pred_data, classes=label_list)
         cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -176,23 +189,25 @@ try:
             #         'img time: {img:.4f} | det put: {put:.4f} | det: {det:.4f} | opt: {opt:.4f} | mid: {mid:.4f} | mlp: {mlp:.4f}'.format(
             #         img=img_read_time, put=det_loader_put_time, det=det_time, opt=det_openpose_time, mid=mid_proc_time, mlp=mlp_pred_time)
             # )
-    print("walk: {}, stand: {}, stop: {}, come:{}, open:{}, phone:{}".format(walk_t, stand_t, stop_t, come_t, open_t, phone_t))        
+    print("walk: {}, stand: {}, stop: {}, come:{}, open:{}, phone:{}".format(walk_t, stand_t, stop_t, come_t, open_t, phone_t))       
+    proc_server.stop() 
     # image_reader.stop()
-    camera_reader.stop()
+    # camera_reader.stop()
     det_loader.stop()
     pose_loader.stop()
     mid_processor.stop()
     act_recognizer.stop()
-    ros_talker.stop()
+    # ros_talker.stop()
 except KeyboardInterrupt:
     print("walk: {}, stand: {}, stop: {}, come:{}, open:{}, phone:{}".format(walk_t, stand_t, stop_t, come_t, open_t, phone_t)) 
+    proc_server.stop() 
     # image_reader.stop()
-    camera_reader.stop()
+    # camera_reader.stop()
     det_loader.stop()
     pose_loader.stop()
     mid_processor.stop()
     act_recognizer.stop()
-    ros_talker.stop()
+    # ros_talker.stop()
 
 
 
---
+++ src/Modules/myserver.py
@@ -0,0 +1,67 @@
+#!/home/dongjai/anaconda3/envs/tensorflow2/bin/python
+import os
+import sys
+from threading import Thread
+from queue import Queue
+import pickle
+
+
+class myServer():
+    def __init__(self, opt, queueSize=128):
+        self.opt = opt
+        self.host = opt.host
+        self.port = opt.port
+
+        if opt.sp:
+            self.data_queue = Queue(maxsize=queueSize)
+            self.outptu_queue = Queue(maxsize=queueSize)
+
+    def start_worker(self, target):
+        if self.opt.sp:
+            p = Thread(target=target, args=(), kwargs={})#target是該線程需要完成的方法函數
+        # else:
+        p.start()
+        return p
+    
+    def start(self):
+        self.server_worker = self.start_worker(self.frame_reader_process)
+        return self
+
+    def stop(self):
+        self.server_worker.join()
+        self.clear_queues()
+
+    def clear_queues(self):
+        self.clear(self.data_queue)
+
+    def clear(self, queue):
+        while not queue.empty():
+            queue.get()
+
+    def wait_and_put(self, queue, item):
+        queue.put(item)
+
+    def wait_and_get(self, queue):
+        return queue.get()
+    
+    def frame_reader_process(self, **kargcs):
+        from multiprocessing.connection import Listener
+        server_sock = Listener((self.host, self.port))
+
+        conn = server_sock.accept()
+        print('Server Listening')
+        while True:
+            data_bytes = conn.recv()
+            data = pickle.loads(data_bytes)
+            # print('Received:', type(data))
+            self.wait_and_put(self.data_queue, data)
+
+            pred_data = self.wait_and_get(self.outptu_queue)
+            pred_data_bytes = pickle.dumps(pred_data)
+            conn.send(pred_data_bytes)
+    
+    def read_data(self):
+        return self.wait_and_get(self.data_queue)
+    
+    def get_data(self, pred_data):
+        self.wait_and_put(self.outptu_queue, pred_data)
